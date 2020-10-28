5f9ca3d011b27e0f9876e77a0b659cf1ed3e9848
Randall Scarberry <rscarberry.ctr@kr.af.mil>
2020-10-28 13:08:57 -0600

    Added lots of comments

--- src/main/java/com/rscarberry/parallelismdemos/DistanceComputationsDemo.java
+++ src/main/java/com/rscarberry/parallelismdemos/DistanceComputationsDemo.java
@@ -3,6 +3,7 @@ package com.rscarberry.parallelismdemos;
 import org.apache.commons.math3.stat.descriptive.SummaryStatistics;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
@@ -12,64 +13,76 @@ import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
+/**
+ * This application calculates euclidean distances of a number of n-dimensional double vectors to
+ * a smaller number of n-dimensional double vectors. This is a task that is performed in data
+ * clustering algorithms sucn as k-means clustering.
+ * <p>
+ *  The purpose of the application is to implement the same distance calculations in a number of different
+ *  ways to compare the performance of the various methods.
+ * </p>
+ */
 public class DistanceComputationsDemo {
 
     public static void main(String[] args) {
 
+        // The number of n-d vectors in the larger list of double arrays.
         int numVectors = 100000;
+        // The number of n-d vectors in the smaller list of double arrays. These are called "centers"
+        // in a nod to k-means clustering, which begins by picking some initial cluster centers.
         int numCenters = 20;
+        // The lengths of the double arrays (the n).
         int vectorLength = 100;
+        // When timing each method, the number of warmup runs before collecting the timing results.
+        // This is to give the JVM a chance to optimize.
         int warmupRuns = 10;
+        // The number of timing runs.
         int timeingRuns = 50;
+        // The number of outer timing loops. In each loop, the various methods are shuffled so in the final
+        // results, the effects of the ordering are averaged out.
         int outerLoops = 10;
 
-        // Create some test data. This is simulating computing the distances between
-        // vectors in N-space and cluster centers. This is a task done in clustering methods,
-        // such as k-means clustering.
+        // Create the data for testing. It's just random data, since it doesn't matter what the actual values are.
         List<double[]> vectors = generateRandomVectors(numVectors, vectorLength, 1234L);
         List<double[]> centers = generateRandomVectors(numCenters, vectorLength, 4567L);
 
+        // A threadpool to use in one or more of the methods.
         ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+        // A list of the tuples containing the calculation method name and callable that routes to the
+        // method computing the distances. The single-threaded entry should always be the first.
         List<Tuple2<String, Callable<List<double[]>>>> callableTuples = new ArrayList<>(List.of(
                 // Have the single-threaded implementation first, because the times of the others
                 // are compared against it.
-                Tuples.of("single-threaded", () -> {
-                    return computeDistancesSingleThreaded(vectors, centers);
-                }),
-                Tuples.of("reactor, per vector", () -> {
-                    return computeDistancesInParallelUsingReactor(vectors, centers);
-                }),
-                Tuples.of("reactor, batch", () -> {
-                    return computeDistancesInParallelUsingReactor2(vectors, centers);
-                }),
-                Tuples.of("parallel streams", () -> {
-                    return computeDistancesInParallelUsingParallelStreams(vectors, centers);
-                }),
-                Tuples.of("completable futures", () -> {
-                    return computesDistancesInParallelUsingCompletableFutures(vectors, centers, executorService);
-                }),
-                Tuples.of("callables", () -> {
-                    return computeDistancesUsingCallables(vectors, centers, executorService);
-                })
+                Tuples.of("single-threaded", () -> computeDistancesSingleThreaded(vectors, centers)),
+                Tuples.of("reactor, per vector", () -> computeDistancesInParallelUsingReactorPerVector(vectors, centers,
+                        ForkJoinPool.commonPool())),
+                Tuples.of("reactor, batch", () -> computeDistancesInParallelUsingReactorBatch(vectors, centers,
+                        ForkJoinPool.commonPool())),
+                Tuples.of("parallel streams", () -> computeDistancesInParallelUsingParallelStreams(vectors, centers)),
+                Tuples.of("completable futures", () -> computesDistancesInParallelUsingCompletableFutures(
+                        vectors, centers)),
+                Tuples.of("callables", () -> computeDistancesUsingCallables(vectors, centers, executorService))
         ));
 
+        // Get the name associated with the single-threaded approach.
         final String singleThreadedName = callableTuples.get(0).getT1();
 
-        // To ensure that all method produce the same results.
+        // To ensure that all method produce the same results. We don't want to time a method
+        // that produces incorrent distances.
         Map<String, List<double[]>> nameToDistances = new HashMap<>();
         // If distances are unequal after the first loop, this will be set to false.
         boolean allEqual = true;
 
         try {
 
+            // To collect the mean times for each method.
             Map<String, List<Double>> timesMap = new HashMap<>();
 
             for (int loop = 0; loop < outerLoops; loop++) {
 
-                // Shuffle, because timing results are influenced by order.
+                // Shuffle, in case timing results are influenced by order.
                 Collections.shuffle(callableTuples);
 
                 for (Tuple2<String, Callable<List<double[]>>> tuple : callableTuples) {
@@ -96,6 +109,7 @@ public class DistanceComputationsDemo {
                     });
 
                     SummaryStatistics summaryStatistics = statsTuple.getT2();
+                    // Display the timing result for this timing run of the method.
                     System.out.printf("%03d: %-30s: min = %d msec, max = %d msec, mean = %4.1f msec%n",
                             loop,
                             name,
@@ -107,6 +121,7 @@ public class DistanceComputationsDemo {
 
                 System.out.println();
 
+                // On the first loop, check that all methods produce the same distances. If not, bail from the loop.
                 if (loop == 0) {
                     allEqual = checkDistancesEqual(singleThreadedName, nameToDistances);
                     // Not needed after first loop.
@@ -119,8 +134,10 @@ public class DistanceComputationsDemo {
             }
 
             // Only show the final timing results if the distances were all equal.
+            //
             if (allEqual) {
 
+                // Compute mean times from the loop means for each method.
                 SortedMap<Double, String> avgTimes = new TreeMap<>();
 
                 for (Map.Entry<String, List<Double>> entry : timesMap.entrySet()) {
@@ -133,16 +150,19 @@ public class DistanceComputationsDemo {
                     avgTimes.put(sum / times.size(), name);
                 }
 
+                // Pick out the entry for the single-threaded method, so that the speedups can be computed.
                 double singleThreadedMs = 0.0;
                 for (Map.Entry<Double, String> entry : avgTimes.entrySet()) {
-                    if (entry.getValue().equals("single-threaded")) {
+                    if (entry.getValue().equals(singleThreadedName)) {
                         singleThreadedMs = entry.getKey();
                         break;
                     }
                 }
 
-                System.out.printf("%nFinal Results:%n");
+                System.out.printf("%nFinal Results (%d Processors):%n%n",
+                        Runtime.getRuntime().availableProcessors());
 
+                // Need it to be stashed in a final for the lambda.
                 final double stm = singleThreadedMs;
 
                 avgTimes.entrySet()
@@ -192,6 +212,19 @@ public class DistanceComputationsDemo {
         return Tuples.of(results, summaryStatistics);
     }
 
+    /**
+     * Computes the distances using a simple single-threaded approach. The results from this method are
+     * regarded as the ground truth results.
+     *
+     * @param vectors a list containing double arrays all of the same length.
+     * @param centers a list containing double arrays all of the same length and also the same
+     *                length as the vectors in the first list.
+     *
+     * @return a list of double arrays representing the distances. The length should be the same as the length of
+     *   the vectors argument. Each double array should be the length of the centers argument. If the return is
+     *   assigned to the variable distances, you many get the distance
+     *   from vector n to cluster m, using {@code distances.get(n)[m]}.
+     */
     public static List<double[]> computeDistancesSingleThreaded(List<double[]> vectors, List<double[]> centers) {
         List<double[]> distances = new ArrayList<>(vectors.size());
         for (double[] vec : vectors) {
@@ -204,16 +237,29 @@ public class DistanceComputationsDemo {
         return distances;
     }
 
-    public static List<double[]> computeDistancesInParallelUsingReactor(
+    /**
+     * Computes exactly the same distance as {@code computeDistancesSingleThreaded()}, but concurrently using
+     * project reactor, with every distance calculation being launched on its own thread.
+     *
+     * @param vectors
+     * @param centers
+     * @param executor
+     * @return
+     */
+    public static List<double[]> computeDistancesInParallelUsingReactorPerVector(
             List<double[]> vectors,
-            List<double[]> centers) {
+            List<double[]> centers,
+            Executor executor) {
 
         final int numCenters = centers.size();
+        final Scheduler schedulerToUse = executor != null ? Schedulers.fromExecutor(executor) : Schedulers.elastic();
 
         List<Tuple2<Integer, double[]>> tuples = Flux.zip(
                 Flux.range(0, vectors.size()), Flux.fromIterable(vectors))
                 .flatMap(tuple -> Mono.just(tuple)
-                        .subscribeOn(Schedulers.parallel())
+                        // I first tried this using Schedulers.parallel(), but the method took even
+                        // longer than the single-threaded approach.
+                        .subscribeOn(schedulerToUse)
                         .map(tpl -> {
                             double[] d = new double[numCenters];
                             for (int i = 0; i < numCenters; i++) {
@@ -230,12 +276,15 @@ public class DistanceComputationsDemo {
         return tuples.stream().map(Tuple2::getT2).collect(Collectors.toList());
     }
 
-    public static List<double[]> computeDistancesInParallelUsingReactor2(
+    public static List<double[]> computeDistancesInParallelUsingReactorBatch(
             List<double[]> vectors,
-            List<double[]> centers) {
+            List<double[]> centers,
+            Executor executor) {
 
         final int numVectors = vectors.size();
         final int numCenters = centers.size();
+
+        final Scheduler scheduler = executor != null ? Schedulers.fromExecutor(executor) : Schedulers.elastic();
 
         final int[] vectorsPerThread = vectorsPerThread(numVectors);
 
@@ -245,31 +294,16 @@ public class DistanceComputationsDemo {
         int startVector = 0;
 
         for (int i = 0; i < numProcessors; i++) {
-
             final int sv = startVector;
             final int nv = vectorsPerThread[i];
-
-            distanceSuppliers.add(() -> {
-                List<double[]> dists = new ArrayList<>(nv);
-                int lim = sv + nv;
-                for (int j = sv; j < lim; j++) {
-                    double[] vec = vectors.get(j);
-                    double[] d = new double[numCenters];
-                    for (int k = 0; k < numCenters; k++) {
-                        d[k] = euclideanDistance(vec, centers.get(k));
-                    }
-                    dists.add(d);
-                }
-                return dists;
-            });
-
+            distanceSuppliers.add(() -> computeDistancesOfRange(vectors, centers, sv, nv));
             startVector += nv;
         }
 
         List<Tuple2<Integer, List<double[]>>> tuples =
                 Flux.zip(Flux.range(0, distanceSuppliers.size()), Flux.fromIterable(distanceSuppliers))
                         .flatMap(tuple -> Mono.just(tuple)
-                                .subscribeOn(Schedulers.parallel())
+                                .subscribeOn(scheduler)
                                 .map(tpl -> Tuples.of(tpl.getT1(), tpl.getT2().get())))
                         .subscribeOn(Schedulers.immediate())
                         .collectList()
@@ -284,42 +318,43 @@ public class DistanceComputationsDemo {
         return distances;
     }
 
+    /**
+     * Computes exactly the same distance as {@code computeDistancesSingleThreaded()}, but concurrently by
+     * parallel streaming through the vectors list, computing the distances via a lambda in map, and
+     * collecting the distances in a new list.
+     * <p>
+     * Not only is this the simplest method, but in my tests on a Linux laptop with 8 processors, it is the
+     * fastest.
+     * </p>
+     *
+     * @param vectors
+     * @param centers
+     *
+     * @return
+     */
     public static List<double[]> computeDistancesInParallelUsingParallelStreams(
             List<double[]> vectors,
             List<double[]> centers) {
 
-        int numVectors = vectors.size();
-        int numCenters = centers.size();
+        final int numCenters = centers.size();
 
-        // Create the distances list and prepopulate with zero vectors.
-        List<double[]> distances = new ArrayList<>(numVectors);
-        for (int i = 0; i < numVectors; i++) {
-            distances.add(new double[numCenters]);
-        }
-
-        IntStream.range(0, vectors.size())
-                // This causes forEach to be called on multiple threads simultaneous. The Intstream is necessary
-                // to preserve the correct order of the distances in the result.
-                .parallel()
-                .forEach(n -> {
-                    double[] vec = vectors.get(n);
-                    double[] dists = distances.get(n);
-                    for (int i = 0; i < numCenters; i++) {
-                        dists[i] = euclideanDistance(vec, centers.get(i));
+        return vectors.parallelStream()
+                .map(v -> {
+                    double[] dists = new double[numCenters];
+                    for (int i=0; i<numCenters; i++) {
+                        dists[i] = euclideanDistance(v, centers.get(i));
                     }
-                });
-
-        return distances;
+                    return dists;
+                })
+                .collect(Collectors.toList());
     }
 
     public static List<double[]> computesDistancesInParallelUsingCompletableFutures(
             List<double[]> vectors,
-            List<double[]> centers,
-            Executor executor
+            List<double[]> centers
     ) throws ExecutionException, InterruptedException {
 
         final int numVectors = vectors.size();
-        final int numCenters = centers.size();
 
         final int[] vectorsPerThread = vectorsPerThread(numVectors);
         final int numThreads = vectorsPerThread.length;
@@ -329,18 +364,9 @@ public class DistanceComputationsDemo {
         for (int i = 0; i < numThreads; i++) {
             final int sv = startVector;
             final int nv = vectorsPerThread[i];
-            completableFutures.add(CompletableFuture.supplyAsync(() -> {
-                List<double[]> dists = new ArrayList<>(nv);
-                int lim = sv + nv;
-                for (int j = sv; j < lim; j++) {
-                    double[] d = new double[numCenters];
-                    for (int k = 0; k < numCenters; k++) {
-                        d[k] = euclideanDistance(vectors.get(j), centers.get(k));
-                    }
-                    dists.add(d);
-                }
-                return dists;
-            }, executor));
+            completableFutures.add(CompletableFuture.supplyAsync(
+                    () -> computeDistancesOfRange(vectors, centers, sv, nv))
+            );
             startVector += nv;
         }
 
@@ -364,7 +390,6 @@ public class DistanceComputationsDemo {
             ExecutorService executorService) throws InterruptedException, ExecutionException {
 
         final int numVectors = vectors.size();
-        final int numCenters = centers.size();
 
         final int[] vectorsPerThread = vectorsPerThread(numVectors);
         final int numThreads = vectorsPerThread.length;
@@ -374,20 +399,7 @@ public class DistanceComputationsDemo {
         for (int i = 0; i < numThreads; i++) {
             final int sv = startVector;
             final int nv = vectorsPerThread[i];
-
-            callables.add(() -> {
-                List<double[]> dists = new ArrayList<>(nv);
-                int lim = sv + nv;
-                for (int j = sv; j < lim; j++) {
-                    double[] d = new double[numCenters];
-                    for (int k = 0; k < numCenters; k++) {
-                        d[k] = euclideanDistance(vectors.get(j), centers.get(k));
-                    }
-                    dists.add(d);
-                }
-                return dists;
-            });
-
+            callables.add(() -> computeDistancesOfRange(vectors, centers, sv, nv));
             startVector += nv;
         }
 
@@ -399,6 +411,22 @@ public class DistanceComputationsDemo {
         }
 
         return distances;
+    }
+
+    private static List<double[]> computeDistancesOfRange(List<double[]> vectors, List<double[]> centers,
+                                                          int startVector, int numVectors) {
+        final int numCenters = centers.size();
+        List<double[]> dists = new ArrayList<>(numVectors);
+        int lim = startVector + numVectors;
+        for (int i = startVector; i < lim; i++) {
+            double[] d = new double[numCenters];
+            double[] vec = vectors.get(i);
+            for (int j = 0; j < numCenters; j++) {
+                d[j] = euclideanDistance(vec, centers.get(j));
+            }
+            dists.add(d);
+        }
+        return dists;
     }
 
     /**
@@ -441,6 +469,12 @@ public class DistanceComputationsDemo {
         return vectors;
     }
 
+    /**
+     * Computes the euclidean distance between two vectors of the same length.
+     * @param vector1
+     * @param vector2
+     * @return the euclidean distance, which should always be >= 0.0.
+     */
     public static double euclideanDistance(double[] vector1, double[] vector2) {
         double sum = 0.0;
         for (int i = 0; i < vector1.length; i++) {
@@ -450,6 +484,13 @@ public class DistanceComputationsDemo {
         return Math.sqrt(sum);
     }
 
+    /**
+     * Checks whether the distances included as values in the map parameter are all equal.
+     *
+     * @param groundTruthName the key into the map
+     * @param distMap
+     * @return
+     */
     public static boolean checkDistancesEqual(String groundTruthName, Map<String, List<double[]>> distMap) {
         List<double[]> trueDistances = distMap.get(groundTruthName);
         AtomicBoolean equal = new AtomicBoolean(true);
